798b5c5f1570754557b15bec1703a5238760abed
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-24 17:52:31 +0300

    Initial Commit

---
+++ SwiftUILoadingView/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUILoadingView
+//
+//  Created by Ramill Ibragimov on 24.06.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUILoadingView/SwiftUILoadingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILoadingViewApp.swift
+//  SwiftUILoadingView
+//
+//  Created by Ramill Ibragimov on 24.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILoadingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
